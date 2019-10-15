1545a80f0bac18f718958fdc87170c0a0760fa08
Takeyuki UEDA <ueda@latelierdueda.com>
2019-10-15 22:34:36 +0900

    lesson 01

--- lib/login_page.dart
+++ lib/login_page.dart
@@ -1,6 +1,15 @@
 import 'package:flutter/material.dart';
 
-class LoginPage extends StatelessWidget {
+class LoginPage extends StatefulWidget {
+  @override
+  _LoginPageState createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+
+  String _email;
+  String _password;
+
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
