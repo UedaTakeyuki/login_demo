893f1c3383cd92b0275e88b023ddf06deb06efb9
Takeyuki UEDA <ueda@latelierdueda.com>
2019-10-16 20:21:30 +0900

    lesson #3

--- lib/login_page.dart
+++ lib/login_page.dart
@@ -6,12 +6,18 @@ class LoginPage extends StatefulWidget {
   _LoginPageState createState() => _LoginPageState();
 }
 
+enum FormType {
+  login,
+  register
+}
+
 class _LoginPageState extends State<LoginPage> {
 
   final formKey = new GlobalKey<FormState>();
 
   String _email;
   String _password;
+  FormType _formType = FormType.login;
 
   bool validateAndSave(){
     final form = formKey.currentState;
@@ -23,10 +29,35 @@ class _LoginPageState extends State<LoginPage> {
     }
   }
 
-  void validateAndSubmit(){
+  void validateAndSubmit() async {
     if(validateAndSave()){
-
+      try {
+        if (_formType == FormType.login) {
+          var user = await FirebaseAuth.instance
+              .signInWithEmailAndPassword(email: _email, password: _password);
+          print('Signed in: $user.uid');
+        } else {
+          var user = await FirebaseAuth.instance.createUserWithEmailAndPassword(email: _email, password: _password);
+          print('Registered user: $user.uid');
+        }
+      } catch (e) {
+        print('Error $e');
+      }
     }
+  }
+
+  void moveToRegister(){
+    formKey.currentState.reset();
+    setState(() {
+      _formType = FormType.register;
+    });
+  }
+
+  void moveToLogin(){
+    formKey.currentState.reset();
+    setState(() {
+      _formType = FormType.login;
+    });
   }
 
   @override
@@ -43,31 +74,59 @@ class _LoginPageState extends State<LoginPage> {
             key: formKey,
             child: new Column(
               crossAxisAlignment: CrossAxisAlignment.stretch,
-              children: <Widget>[
-                new TextFormField(
-                  decoration: new InputDecoration(
-                    labelText: 'Email',
-                  ),
-                  validator: (value) => value.isEmpty ? 'Email can\'t be empty' : null,
-                  onSaved: (value) => _email = value,
-                ),
-                new TextFormField(
-                  decoration: new InputDecoration(
-                    labelText: 'Password',
-                  ),
-                  validator: (value) => value.isEmpty ? 'Password can\'t be empty' : null,
-                  obscureText: true,
-                  onSaved: (value) => _password = value,
-                ),
-                new RaisedButton(
-                  onPressed: validateAndSubmit,
-                  child: new Text('Login', style: new TextStyle(fontSize: 20.0),),
-                )
-              ],
+              children: buildInputs() + buildSubmitButtons(),
             ),
           ),
         ),
       ),
     );
+  }
+
+  List<Widget> buildInputs(){
+    return [
+      new TextFormField(
+        decoration: new InputDecoration(
+          labelText: 'Email',
+        ),
+        validator: (value) => value.isEmpty ? 'Email can\'t be empty' : null,
+        onSaved: (value) => _email = value,
+      ),
+      new TextFormField(
+        decoration: new InputDecoration(
+          labelText: 'Password',
+        ),
+        validator: (value) => value.isEmpty ? 'Password can\'t be empty' : null,
+        obscureText: true,
+        onSaved: (value) => _password = value,
+      ),
+    ];
+  }
+
+  List<Widget> buildSubmitButtons(){
+    if (_formType == FormType.login) {
+      return [
+        new RaisedButton(
+          onPressed: validateAndSubmit,
+          child: new Text('Login', style: new TextStyle(fontSize: 20.0),),
+        ),
+        new FlatButton(
+          onPressed: moveToRegister,
+          child: new Text(
+            'Create an account', style: new TextStyle(fontSize: 20.0),),
+        )
+      ];
+    } else {
+      return [
+        new RaisedButton(
+          onPressed: validateAndSubmit,
+          child: new Text('Create an account', style: new TextStyle(fontSize: 20.0),),
+        ),
+        new FlatButton(
+          onPressed: moveToLogin,
+          child: new Text(
+            'Have an account? Login', style: new TextStyle(fontSize: 20.0),),
+        )
+      ];
+    }
   }
 }
