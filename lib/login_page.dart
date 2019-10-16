f06ed3ee782c8a93e9af36b366987b28ea8412d9
Takeyuki UEDA <ueda@latelierdueda.com>
2019-10-16 17:35:32 +0900

    lesson #2

--- lib/login_page.dart
+++ lib/login_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 
 class LoginPage extends StatefulWidget {
   @override
@@ -7,8 +8,26 @@ class LoginPage extends StatefulWidget {
 
 class _LoginPageState extends State<LoginPage> {
 
+  final formKey = new GlobalKey<FormState>();
+
   String _email;
   String _password;
+
+  bool validateAndSave(){
+    final form = formKey.currentState;
+    if (form.validate()){
+      form.save();
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  void validateAndSubmit(){
+    if(validateAndSave()){
+
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -18,7 +37,36 @@ class _LoginPageState extends State<LoginPage> {
         title: new Text('flutter login demo'),
       ),
       body: new Container(
-        child: new Text('Hello World'),
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: new Form(
+            key: formKey,
+            child: new Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: <Widget>[
+                new TextFormField(
+                  decoration: new InputDecoration(
+                    labelText: 'Email',
+                  ),
+                  validator: (value) => value.isEmpty ? 'Email can\'t be empty' : null,
+                  onSaved: (value) => _email = value,
+                ),
+                new TextFormField(
+                  decoration: new InputDecoration(
+                    labelText: 'Password',
+                  ),
+                  validator: (value) => value.isEmpty ? 'Password can\'t be empty' : null,
+                  obscureText: true,
+                  onSaved: (value) => _password = value,
+                ),
+                new RaisedButton(
+                  onPressed: validateAndSubmit,
+                  child: new Text('Login', style: new TextStyle(fontSize: 20.0),),
+                )
+              ],
+            ),
+          ),
+        ),
       ),
     );
   }
